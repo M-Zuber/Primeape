fd0eb228a58f9c964ead05d0fba614e0611b5ca3
Mordechai Zuber <mez613@gmail.com>
2019-10-10 18:26:52 +0300

    Add some tests

---
+++ Primeape/UnitTests/TestPokemon.cs
@@ -0,0 +1,27 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class DualType : Pokemon
+    {
+        public override string Name => nameof(DualType);
+
+        public override Models.Type PrimaryType => Models.Type.Fighting;
+
+        public override Models.Type? SecondaryType => Models.Type.Flying;
+    }
+
+    public class SingleType : Pokemon
+    {
+        public override string Name => nameof(DualType);
+
+        public override Models.Type PrimaryType => Models.Type.Fighting;
+
+        public override Models.Type? SecondaryType => null;
+    }
+}
